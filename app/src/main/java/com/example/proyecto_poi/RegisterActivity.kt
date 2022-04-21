4d44ab84f2c25b825065550aa4defd688bbcd0ba
Amador-Rodriguez <82629742+Amador-Rodriguez@users.noreply.github.com>
2022-04-20 21:11:48 -0500

    Arreglar problema de JSON

--- app/src/main/java/com/example/proyecto_poi/RegisterActivity.kt
+++ app/src/main/java/com/example/proyecto_poi/RegisterActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
+
 import java.io.StringReader
+import java.net.URL
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -17,18 +23,6 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
     }
 
-    private fun insertar (){
-        val url = "http://localhost/db_psm/register_inc.php";
-        val datos = HashMap<String, Any>()
-        val txt_name = findViewById<View>(R.id.txt_name) as EditText
-        val txt_pwd = findViewById<View>(R.id.txt_pwd) as EditText
 
-        data["nombre"] = txt_name.text.toString()
-        data["contrasena"] = txt_pwd.text.toString()
-
-        val datos_enviar = JSONObject(datos as Map<*, *>?)
-
-
-    }
 
 }
